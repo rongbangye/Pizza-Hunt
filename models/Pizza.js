49406237d32438567cc3d9ad1f9a7971f38f4e7f
rongbang <rongbang.ye@gmail.com>
2020-11-21 18:43:09 -0800

    added getAllPizzas and created connection between pizza and comment dispaly that in the homepage

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,5 +1,6 @@
 // create the Pizza model using the PizzaSchema
 const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
 const PizzaSchema = new Schema(
   {
@@ -12,6 +13,7 @@ const PizzaSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
     },
     size: {
       type: String,
@@ -31,10 +33,16 @@ const PizzaSchema = new Schema(
   {
     toJSON: {
       virtuals: true,
+      getters: true,
     },
     id: false,
   }
 );
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.length;
+});
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model("Pizza", PizzaSchema);
